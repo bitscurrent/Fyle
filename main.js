c030827c86cc3ab7300db858e55a12654a7d0073
Dilip <dilipboidya.office@gmail.com>
2024-06-03 23:54:07 +0530

    fix css

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("OMG");
+const contactButton = document.getElementById("contactUsButton");
+const popupForm = document.getElementById("popupForm");
+const closeButton = document.getElementById("closeButton");
+const cancelButton = document.getElementById("cancel");
+
+const showPopupForm = () => {
+  popupForm.style.display = "flex"; // Show the form
+};
+
+const hidePopupForm = () => {
+  popupForm.style.display = "none"; // Hide the form
+};
+
+contactButton.addEventListener("click", showPopupForm);
+closeButton.addEventListener("click", hidePopupForm);
+cancelButton.addEventListener("click", hidePopupForm);
+
+// Hide the form if the user clicks outside of it
+window.addEventListener("click", (event) => {
+  if (event.target === popupForm) {
+    hidePopupForm();
+  }
+});
